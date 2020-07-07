b2dbcaf67217fe31cb2886c0d6b3b7d6ceb429d1
Jared Davidson (Archetapp) <archetapp@gmail.com>
2020-07-07 16:59:05 -0600

    Initial Commit

---
+++ TrySomethingNew/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrySomethingNew
+//
+//  Created by Jared on 7/7/20.
+//  Copyright © 2020 Archetapp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
